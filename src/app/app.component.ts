ce592b2e2215b539c0b99f3382e6d7da8a744f69
Mohamed-Kamash <Mohamedes.Kamash@gmail.com>
2024-04-19 15:32:45 +0200

    version 0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './component/nav/nav.component';
 import { IdComponent } from './component/id/id.component';
+import { IntroComponent } from './component/intro/intro.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet , NavComponent , IdComponent],
+  imports: [CommonModule, RouterOutlet , NavComponent , IdComponent , IntroComponent ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
