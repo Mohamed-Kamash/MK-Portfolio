84cf3ce096d827f23cdc9d28bbc388d67d227c47
Mohamed-Kamash <mohamedes.kamash@gmail.com>
2024-04-21 14:11:42 +0200

    version 0.1

--- src/app/component/intro/intro.component.ts
+++ src/app/component/intro/intro.component.ts
@@ -1,13 +1,57 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import Typed from 'typed.js';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { timeout } from 'rxjs';
+
 
 @Component({
   selector: 'app-intro',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './intro.component.html',
-  styleUrls: ['./intro.component.scss']
+  styleUrls: ['./intro.component.scss'],
+  animations: [
+    trigger('fadeIntro', [
+      state('closed', style({
+        opacity: 0,
+      })),
+      transition("* => closed", [
+        animate('1000ms')
+      ]),
+    ]),
+    trigger("slideUp",[
+      state('slide',style({transform:"translateY(-100%)"})),
+      transition("*=>slide",[
+        animate("1000ms")
+      ])
+    ])
+  ]
 })
 export class IntroComponent {
+
+    fadeIntro:Boolean=false
+    slide:boolean=false
+    slideUp():void{
+      setTimeout(() => {this.slide = true}, 1200);
+    }
+
+    ngAfterViewInit() {
+    let typed = new Typed('#element', {
+      strings:['ohamed Essam' ,'ohamed Kamash'],
+      backSpeed:40,
+      backDelay:70,
+      startDelay:1640,
+      typeSpeed: 44,
+      showCursor:false
+    });
+
+
+    setTimeout(() => {
+      this.fadeIntro = true
+      this.slideUp()
+    }, 5100);
+
+  }
 
 }
--- src/app/component/projects/projects.component.ts
+++ src/app/component/projects/projects.component.ts
@@ -1,14 +1,13 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
-
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [CommonModule,CarouselModule],
+  imports: [CommonModule, CarouselModule],
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent {
   customOptions: OwlOptions = {
@@ -18,33 +17,29 @@ export class ProjectsComponent {
     pullDrag: true,
     dots: false,
     navSpeed: 700,
-    margin:0,
-    autoWidth:true,
+    margin: 0,
+    autoWidth: true,
     navText: ['<', '>'],
     responsive: {
       0: {
         items: 1,
         nav: false,
         dots: true,
-
       },
       576: {
         items: 1,
         nav: false,
         dots: true,
-
       },
       768: {
         items: 2,
-        nav: true
-
+        nav: true,
       },
       992: {
         items: 2,
-        nav: true
+        nav: true,
       },
     },
     // nav: true
-  }
-
+  };
 }
