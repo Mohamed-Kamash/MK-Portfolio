d7175692796b3f44a3cb69977dcdc1af9494b329
Mohamed-Kamash <Mohamedes.Kamash@gmail.com>
2024-04-28 14:35:25 +0300

    align nav

--- src/app/component/id/id.component.ts
+++ src/app/component/id/id.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
@@ -7,9 +7,29 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './id.component.html',
-  styleUrls: ['./id.component.scss']
+  styleUrls: ['./id.component.scss'],
+  
 })
 export class IdComponent {
+
+  coppied:boolean = false
+
+  @ViewChild('mytext') text!:ElementRef
+
+  copy(){
+    let coppiedText = this.text.nativeElement.innerHTML
+    navigator.clipboard.writeText(coppiedText).then(() => {
+      console.log('Content copied to clipboard ' + coppiedText);
+    })
+    this.coppied=true
+    this.hideCoppiedEle()
+  }
+
+  hideCoppiedEle():void{
+    setTimeout(() => {
+      this.coppied = false
+    }, 1000);
+  }
 
 
 }
